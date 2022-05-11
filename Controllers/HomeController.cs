c1ad3e107675fcb097a0c0297bd764541a275378
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-05-11 09:49:24 +0700

    Add controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Data;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,36 +13,28 @@ namespace SQL_Injection.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // GET: api/<HomeController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            conn.Open();
+            SqlCommand command = new SqlCommand("SELECT TOP(10) * FROM DimEmployee", conn);
+
+            //command.Parameters.AddWithValue("@zip", "india");
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                dt.Load(reader);
+            }
+            return Ok();
         }
 
-        // GET api/<HomeController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<HomeController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<HomeController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<HomeController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
